3cdd03a9189f6fe22fabe11ede0017ccf5a11b76
Summer <nathandouglaslunn@gmail.com>
2022-08-02 19:28:48 -0400

    add Login.js and Register.js components

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,18 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import './App.css';
 import Todos from './Todos';
+import Login from './Login';
+import Register from './Register';
 
 function App() {
+  const [state, setState] = useState({
+    user: {},
+    todos: [],
+    signingUp: false,
+  })
   const [todos, setTodos] = useState([]);
+  const [user, setUser] = useState({});
+  const [signingUp, setSigningUp] = useState(false);
 
   useEffect(() => {
     axios.get('http://localhost:8080/api/todos')
@@ -15,7 +24,9 @@ function App() {
 
   return (
     <div className="App">
-      <Todos todos={todos} setTodos={setTodos}/>
+      {!user.username && !signingUp && <Login setUser={setUser} setSigningUp={setSigningUp}/>}
+      {!user.username && signingUp && <Register setUser={setUser} setSigningUp={setSigningUp}/>}
+      {user.username && <Todos todos={todos} setTodos={setTodos}/>}
     </div>
   );
 }
