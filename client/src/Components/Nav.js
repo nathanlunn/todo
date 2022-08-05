892e50dab53c639b66043fc59011c0f41284348a
Summer <nathandouglaslunn@gmail.com>
2022-08-04 21:29:38 -0400

    begin styling on nav buttons

--- client/src/Components/Nav.js
+++ client/src/Components/Nav.js
@@ -11,10 +11,10 @@ export default function Nav({state, setState}) {
         <h3>Todo List App</h3>
       </div>
 
-      <div className="nav__button">
-        {state.user.username && <button className="nav__button--logout" onClick={() => {setState(prev => ({...prev, user:{}}))}}>Logout</button>}
-        {!state.user.username && !state.signingUp && <button className="nav__button--singup" onClick={() => {setState(prev => ({...prev, signingUp: true}))}}>Sign Up</button>}
-        {!state.user.username && state.signingUp && <button className="nav__button--login" onClick={() => {setState(prev => ({...prev, signingUp: false}))}}>Login</button>}
+      <div>
+        {state.user.username && <button className="nav__button nav__button--logout" onClick={() => {setState(prev => ({...prev, user:{}}))}}>Logout</button>}
+        {!state.user.username && !state.signingUp && <button className="nav__button nav__button--singup" onClick={() => {setState(prev => ({...prev, signingUp: true}))}}>Sign Up</button>}
+        {!state.user.username && state.signingUp && <button className="nav__button nav__button--login" onClick={() => {setState(prev => ({...prev, signingUp: false}))}}>Login</button>}
       </div>
 
     </div>
